62c5443e40fe1cfd80ec40d7f1e19e1ffd6fef21
luisag98 <44959559+luisag98@users.noreply.github.com>
2018-11-11 23:46:54 -0700

    Add files via upload

---
+++ Eva2_1_INTRO_CONSTRUCTORES/src/Principal.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author invitado
+ */
+public class Principal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Principal pObjPpal=new Principal();
+        Prueba pObjPrue=new Prueba();
+        System.out.println(pObjPrue.X);
+    }
+    
+}
+
+class Prueba{
+    int X;
+    public Prueba(){
+        X = 100;
+    }
+}
